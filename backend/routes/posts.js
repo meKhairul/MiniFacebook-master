a400757cb7a6d3155e7896451b36dae8f0438486
Khairul Alam <bsse1118@iit.du.ac.bd>
2022-06-30 12:30:36 +0600

    backend updated

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -18,4 +18,10 @@ router.post('/',(req,res)=>{
     });
 });
 
+router.get('/',async(req,res)=>{
+    const posts = await Post.find();
+	res.send(posts);
+});
+
+
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,23 +3,48 @@ var router = express.Router();
 var User = require('../models/user');
 var jwt = require('jsonwebtoken');
 
-router.post('/',(req,res)=>{
-    var user = new User({
+router.post('/',async(req,res)=>{
+    var new_user = new User({
         name:req.body.name,
         email:req.body.email,
         password:User.hashPassword(req.body.password),
         creation_date: Date.now()
     });
-    let promise = user.save();
 
-    promise.then((doc)=>{
-        return res.status(201).json(doc);
-    })
+    const users = await User.find();
+    var isDuplicate = false;
 
-    promise.catch((err)=>{
-        return res.status(501).json({message:'Error Registering'})
-    })
+    for(let user of users)
+    {
+        if(user.email == new_user.email)
+        {
+            isDuplicate = true;
+        }
+    }
+    
+    if(!isDuplicate)
+    {
+        let promise = new_user.save();
 
+        promise.then((doc)=>{
+            return res.status(201).json(doc);
+        })
+
+        promise.catch((err)=>{
+            return res.status(501).json({message:'Error Registering'})
+        })
+    }
+    else
+    {
+        res.status(501).json({message:'This email already has an account'})
+    }
+    
+
+});
+
+router.get('/',async(req,res)=>{
+    const users = await User.find();
+	res.send(users);
 });
 
 module.exports = router;
